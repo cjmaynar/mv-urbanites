63f4561c52e57e80e2b083c6784f5f1aa673070b
Corey Maynard <me@coreymaynard.com>
2013-03-20 22:17:18 -0400

    Remove the import of now missing url files

--- mvurbanites/mvurbanites/urls.py
+++ mvurbanites/mvurbanites/urls.py
@@ -4,10 +4,6 @@ from django.contrib import admin
 from mvurbanites.views import Home
 from feature import urls as feature_urls
 from blog import urls as blog_urls
-from community import urls as community_urls
-from impact import urls as impact_urls
-from join import urls as join_urls
-from support import urls as support_urls
 
 admin.autodiscover()
 
