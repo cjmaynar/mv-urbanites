a28875db5ad5ef10ef76c80ec04df852be5de3fa
Corey Maynard <me@coreymaynard.com>
2013-03-21 22:02:04 -0400

    Add a fixture to build the four sections, and add begining templates for each. Remove the template model

--- mvurbanites/page/models.py
+++ mvurbanites/page/models.py
@@ -5,7 +5,6 @@ from django.template.defaultfilters import slugify
 class Page(models.Model):
     slug = models.SlugField(blank=True, editable=False)
     title = models.CharField(max_length=255)
-    template = models.ForeignKey('Template')
     parent = models.ForeignKey('self', blank=True, null=True)
     content = models.TextField()
 
@@ -17,9 +16,3 @@ class Page(models.Model):
             self.slug = slugify(self.title)
 
         super(Page, self).save(*args, **kwargs)
-
-class Template(models.Model):
-    name = models.CharField(max_length=255)
-
-    def __unicode__(self):
-        return self.name
