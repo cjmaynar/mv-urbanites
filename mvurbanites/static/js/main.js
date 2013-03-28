b6bacc49b6a0cb4004ef8a0d26c2cd62313e0d31
Corey Maynard <me@coreymaynard.com>
2013-03-28 18:28:26 -0400

    So, we're using bootstrap now, that's kinda cool. I think I like it

--- mvurbanites/static/js/main.js
+++ mvurbanites/static/js/main.js
@@ -1,31 +1,3 @@
 $(function() {
-    /*
-     * These are the words that will cycle on the home page.
-     * */
-    var words = [
-        'Explore',
-        'Improve',
-        'Support',
-        'Discover',
-        'Inform'
-    ];
-
-    $("#slides").slidesjs({
-        width: 960,
-        height: 400,
-        play: {
-            auto: true,
-            pauseOnHover: true,
-        },
-        navigation: {
-            active: false,
-            effect: 'slide'
-        },
-        callback: {
-            start: function(index) {
-                // Index is 1 based, array 0 based
-                $("#focus").html(words[index - 1]);
-            }
-        }
-    })
+    $('#promo').carousel();
 });
