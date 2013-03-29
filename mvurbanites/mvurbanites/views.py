66952fc83e54cc8bc322fbf3b737a62eb70268aa
Corey Maynard <me@coreymaynard.com>
2013-03-29 00:01:58 -0400

    Add the events from Meetup to the calendar

--- mvurbanites/mvurbanites/views.py
+++ mvurbanites/mvurbanites/views.py
@@ -1,4 +1,6 @@
 import requests
+import datetime
+import pprint
 
 from django.shortcuts import render
 from django.views.generic.base import View
@@ -9,14 +11,21 @@ from feature.models import Feature
 from blog.models import Blog
 from page.models import Page
 
-
 class Home(View):
     def get(self, request, *args, **kwargs):
         api = 'https://api.meetup.com/2/events?'
         key = 'key=%s' % (MEETUP_KEY)
         query = '&sign=true&group_urlname=20s-30sMountVernonUrbanites'
-        events = requests.get('%s%s%s' % (api, key, query)).json()
         sections = Page.objects.filter(parent=None)
+
+        tmp_event = []
+        events = requests.get('%s%s%s' % (api, key, query)).json()
+        for event in events['results']:
+            d = datetime.datetime.fromtimestamp(event['time'] / 1e3)
+            event['date'] = "%d/%d/%d" % (d.month, d.day, d.year)
+            tmp_event.append(event)
+
+        events = tmp_event
 
         try:
             blog = Blog.objects.latest()
--- mvurbanites/static/js/main.js
+++ mvurbanites/static/js/main.js
@@ -1,5 +1,36 @@
 $(function() {
     $('#promo').carousel();
 
-    $("#upcoming").datepicker();
+    $("#upcoming").datepicker({
+        beforeShowDay: function(date) {
+            var result = [true, '', null];
+            var matching = $.grep(events, function(event) {
+                return event.Date.valueOf() === date.valueOf();
+            });
+            
+            if (matching.length) {
+                result = [true, 'highlight', null];
+            }
+            return result;
+        },
+        onSelect: function(dateText) {
+            var date,
+                selectedDate = new Date(dateText),
+                i = 0,
+                event = null;
+            
+            while (i < events.length && !event) {
+                date = events[i].Date;
+
+                if (selectedDate.valueOf() === date.valueOf()) {
+                    event = events[i];
+                }
+                i++;
+            }
+            if (event) {
+                alert(event.Title);
+            }
+        }
+    });
+
 });
